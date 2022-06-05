d3307857a8ad9fbafebcfb9737d3d238cd657304
Lê Ngọc Hưng <96043346+sky-hungln@users.noreply.github.com>
2022-06-05 20:50:33 +0700

    base-project-and-layout-add-category

---
+++ src/views/category/AddCategory.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h3 class="pt-3">Add Category</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form>
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" v-model="categoryName" />
+          </div>
+          <div class="form-group">
+            <label>Description</label>
+            <textarea type="text" class="form-control" v-model="description" />
+          </div>
+          <div class="form-group">
+            <label>Image</label>
+            <input type="text" class="form-control" v-model="imageUrl" />
+          </div>
+          <button type="button" class="btn btn-primary" @click="addCategory">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div class="col-3"></div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      categoryName: "",
+      description: "",
+      imageUrl: "",
+    };
+  },
+  methods: {
+      addCategory(){
+          console.log(this.categoryName)
+      }
+  }
+};
+</script>
